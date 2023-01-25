166e178d043f791c9d719214c0c9c4c582386b7b
Riddhi Ranjithkumar <rr238@duke.edu>
2023-01-25 12:25:03 -0500

    Consolidate interfaces

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,12 +4,18 @@ def interface():
     while keep_running:
         print("Options")
         print("1 - Cholestrol")
+        print("2 - HDL")
+        print("3 - LDL")
         print("9 - Quit")
         choice = input("Select an option")
         if choice == "9":
             keep_running = False
         elif choice == "1":
             cholestrol_driver()
+        elif choice =="2":
+            HDL_driver()
+        elif choice == "3":
+            LDL_driver()
     print("Program ending") 
 
 def cholestrol_driver():
@@ -34,22 +40,6 @@ def cholestrol_analysis(cholestrol_int):
 def cholestrol_output(cholestrol_value, cholestrol_analy):
     print("The Cholestrol result of {} is considered {}".format(cholestrol_value, cholestrol_analy))
 
-interface()
-
-def interface():
-    print("Blood calculator")
-    keep_running =True
-    while keep_running:
-        print("Options")
-        print("1 - LDL")
-        print("9 - Quit")
-        choice = input("Select an option")
-        if choice == "9":
-            keep_running = False
-        elif choice == "1":
-            LDL_driver()
-    print("Program ending") 
-
 def LDL_driver():
     LDL_in = LDL_input()
     LDL_analy = LDL_analysis(LDL_in)
@@ -73,22 +63,6 @@ def LDL_analysis(LDL_int):
 
 def LDL_output(LDL_value, LDL_analy):
     print("The LDL result of {} is considered {}".format(LDL_value, LDL_analy))
-
-interface()
-
-def interface():
-    print("Blood calculator")
-    keep_running =True
-    while keep_running:
-        print("Options")
-        print("1 - HDL")
-        print("9 - Quit")
-        choice = input("Select an option")
-        if choice == "9":
-            keep_running = False
-        elif choice == "1":
-            HDL_driver()
-    print("Program ending") 
 
 def HDL_driver():
     HDL_in = HDL_input()
