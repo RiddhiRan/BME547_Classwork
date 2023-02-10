47c5d64a6f26e7fa5e338c95e91fe6e97ca4d736
Riddhi Ranjithkumar <rr238@duke.edu>
2023-02-10 12:55:03 -0500

    changes to db

--- database.py
+++ database.py
@@ -5,10 +5,10 @@ def create_patient_entry(first_name, last_name, patient_mrn, patient_age):
     return new_patient
 
 def main_driver():
-    db = []
-    db.append(create_patient_entry("Ann","Ables", 1, 34))
-    db.append(create_patient_entry("Bob", "Boyles", 2, 45))
-    db.append(create_patient_entry("Chris", "Chou", 3, 52))
+    db = {}
+    db[1] = (create_patient_entry("Ann","Ables", 1, 34))
+    db[2] = (create_patient_entry("Bob", "Boyles", 2, 45))
+    db[3] = (create_patient_entry("Chris", "Chou", 3, 52))
     print(db)
     add_test_to_patient(db, 1, "HDL", 120)
     add_test_to_patient(db, 2, "LDL", 100)
@@ -18,15 +18,15 @@ def main_driver():
 
 def print_directory(db, room_numbers):
     for i, patient in enumerate(db):
-        print("Patient {} is in room {}".format(patient["First Name"], room_numbers[i]))
+        print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
     for patient, rn in zip(db, room_numbers):
-        print("Patient {} is in room {}".format(patient["First Name"], rn))
+        print("Patient {} is in room {}".format(patient[0], rn))
         
 def get_patient_entry(db, mrn_to_find):
-    for patient in db:
-        if patient["MRN"] == mrn_to_find:
-            return patient
-    return False
+    patient = db.get(mrn_to_find)
+    if patient is None:
+        return False
+    return patient
 
 def add_test_to_patient(db, mrn_to_find, test_name, test_value):
     patient = get_patient_entry(db, mrn_to_find)
@@ -56,7 +56,7 @@ def get_full_name(patient):
     return "{} {}".format(patient["First Name"], patient["Last Name"])
 
 def print_database(db):
-    for patient in db:
+    for patient in db.values():
         print("MRN: {}, Full Name: {}, Age: {}".format(patient["MRN"],
                                                        get_full_name(patient), 
                                                        patient["Age"]))
