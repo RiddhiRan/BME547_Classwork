d812719f13271b93d6751c3a7b11484c207b3b5d
Riddhi Ranjithkumar <rr238@duke.edu>
2023-02-17 07:33:52 -0500

    Functioning code to find 3rd coordinate

--- inclass_ex0203.py
+++ inclass_ex0203.py
@@ -6,8 +6,26 @@ def find_gradient(coord1, coord2):
     m = (y2 - y1)/(x2-x1)
     return m
 
+
 def find_y_intercept(m, coord1):
     x1 = coord1[0]
-    y1 = coord1[2]
+    y1 = coord1[1]
     c = y1 - (m*x1)
     return c
+
+
+def find_y(x, m, c):
+    y = m*x + c
+    return y
+
+
+def find_coordinate(coord1, coord2, x):
+    m = find_gradient(coord1, coord2)
+    c = find_y_intercept(m, coord1)
+    y = find_y(x, m, c)
+    print(y)
+    return y
+
+
+if __name__ == "__main__":
+    find_coordinate((1, 1), (2, 2), 3)
--- test_inclass_ex0203.py
+++ test_inclass_ex0203.py
@@ -1,13 +1,34 @@
 import pytest
 
+
 @pytest.mark.parametrize("coordinate1, coordinate2, expected",
-                         [([1, 1], [2, 2], 1),
-                          ([3, 2], [11, 6], 0.5),
+                         [((1, 1), (2, 2), 1),
+                          ((3, 2), (11, 6), 0.5),
                           ])
 def test_find_gradient(coordinate1, coordinate2, expected):
     from inclass_ex0203 import find_gradient
     answer = find_gradient(coordinate1, coordinate2)
     # Assert
     assert answer == expected
-    
-    
+
+
+@pytest.mark.parametrize("m, coordinate1, expected",
+                         [(1, (1, 1), 0),
+                          (3, (2, 7), 1),
+                          ])
+def test_find_y_intercept(m, coordinate1, expected):
+    from inclass_ex0203 import find_y_intercept
+    answer = find_y_intercept(m, coordinate1)
+    # Assert
+    assert answer == expected
+
+
+@pytest.mark.parametrize("x, m, c, expected",
+                         [(1, 2, 1, 3),
+                          (3, 2, 7, 13),
+                          ])
+def test_find_y(x, m, c, expected):
+    from inclass_ex0203 import find_y
+    answer = find_y(x, m, c)
+    # Assert
+    assert answer == expected
